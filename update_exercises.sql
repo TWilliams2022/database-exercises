231ac3aebbcd082eb6b9f404cefafa4b168b7d4b
Tyreik Williams <tyreik.mm.williams@gmail.com>
2022-05-02 15:49:54 -0400

    updates in mySQL

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,7 +1,14 @@
 USE codeup_test_db;
 
+# SELECT *
+# FROM albums
+# WHERE release_date < '1980';
 
+SELECT *
+FROM albums;
 
 SELECT *
 FROM albums
-WHERE release_date > '1980';
+WHERE artist = 'Micheal Jackson';
+
+
