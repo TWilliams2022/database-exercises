d7bac5425471b5c7885a3dfbc6dc9a01c2b5356d
Tyreik Williams <tyreik.mm.williams@gmail.com>
2022-05-02 13:20:12 -0400

    tables refurbed

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,12 +1,14 @@
 USE codeup_test_db;
 
+DROP TABLE IF EXISTS albums;
+
 CREATE TABLE IF NOT EXISTS albums
 (
         id INT UNSIGNED NOT NULL AUTO_INCREMENT primary key ,
-        artist VARCHAR(100) NOT NULL,
-        album_name VARCHAR(100) NOT NULL,
-        release_date DATE NOT NULL,
-        sales FLOAT,
+        artist VARCHAR(200) NOT NULL,
+        album_name VARCHAR(200) NOT NULL,
+        release_date INT UNSIGNED NOT NULL,
+        sales FLOAT ,
         genre VARCHAR(220) NOT NULL
 );
 
